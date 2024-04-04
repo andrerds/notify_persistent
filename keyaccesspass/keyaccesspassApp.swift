dfcdaa6fae24b0f7021556c8d263d985b526db7a
André <andrerds.souza@gmail.com>
2024-04-04 15:37:03 -0300

    Initial Commit

---
+++ keyaccesspass/keyaccesspassApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyaccesspassApp.swift
+//  keyaccesspass
+//
+//  Created by André de Souza on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct keyaccesspassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
